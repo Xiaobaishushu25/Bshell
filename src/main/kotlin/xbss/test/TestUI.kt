d16d26b2586996ca517a45d7cc234bc038fdf5fa
Xiaobaishushu <2055761346@qq.com>
2023-05-17 17:43:42 +0800

    some optimizations

--- src/main/kotlin/xbss/test/TestUI.kt
+++ src/main/kotlin/xbss/test/TestUI.kt
@@ -16,39 +16,7 @@ import kotlin.concurrent.timerTask
 class TestSSH2:Application() {
     override fun start(primaryStage: Stage?) {
         val stage = primaryStage!!
-//        val popName = PopName("就文件夹名")
-//        popName.start(Stage())
-//        popName.textObservable.addListener { _,_,newValue ->
-//            println(newValue)
-//        }
-//        val button = Button("右击")
-//        val treeAreaRightMenu = TreeAreaRightMenu()
-//        button.setOnContextMenuRequested {
-//            treeAreaRightMenu.show(button, it.screenX+3.0, it.screenY+3.0)
-//        }
-//        val fileIOPane = FileIOPane(
-//            FileIOPane.FileIOType.UPLOAD,
-//            "Test",
-//            SimpleStringProperty("demo.py"),
-//            SimpleIntegerProperty(1),
-//            15,
-//            SimpleDoubleProperty(0.3),
-//            "nihaosdasd/tyet"
-//        )
-//        val anchorPane = AnchorPane(button)
-//        stage.scene = Scene(anchorPane,900.0,700.0).apply { stylesheets.add(this::class.java.getResource("/css/xbss.css").toExternalForm()) }
-        val timerTask = timerTask {
-            println(stage.isFocused)
-            Platform.runLater {  stage.requestFocus() }
-
-//            if (!stage.isFocused) {
-//                println("进来了")
-//                Platform.runLater {  stage.requestFocus() }
-//            }
-        }
-        val timer = Timer()
-        timer.schedule(timerTask,5000,5000)
-        stage.show()
+        //这是测试
     }
 }
 
